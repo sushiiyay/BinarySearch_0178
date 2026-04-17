7989f8934906e613b93bec71e0cd54ad549cc30a
sushiiyay <nfa.aliya7811@gmail.com>
2026-04-17 15:10:35 +0700

    Menambahkan for void display

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -56,5 +56,18 @@ void display()
     cout << "   Elemen Array Setelah Diurutkan (Asc)\n";
     cout << "=======================================\n";
 
-    
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
+}
+
+void binarySearch()
+{
+
 }
