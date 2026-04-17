75d894993d02dcfb2a92b342a3f9275dff3c30f6
sushiiyay <nfa.aliya7811@gmail.com>
2026-04-17 14:40:41 +0700

    Menambahkan void input

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+
+int element[10];
+int nPanjang;
+int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+    }
+}
